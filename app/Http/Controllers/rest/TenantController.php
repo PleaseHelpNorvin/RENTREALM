694d3c2ca384326b97a9a27dbe2718a431976a0d
PleaseHelpNorvin <norvincrujido90@gmail.com>
2025-01-10 21:52:53 +0800

    Tenant Preparations

---
+++ app/Http/Controllers/rest/TenantController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\rest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TenantController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public function show(Request $request, $id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function delete(Request $request, $id)
+    {
+        
+    }
+}
